b7b01c153210a501a2505fa1bb4987f13fa34c1f
Lisa <fhs45899@fh-salzburg.ac.at>
2024-04-12 11:30:09 +0200

    Style h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-      <h1 className="text-3xl font-bold underline text-blue">
+      <h1 className="text-h1  font-bold underline text-primary">
         Hello world!
       </h1>
   )
