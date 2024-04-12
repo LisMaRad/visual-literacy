6383f45355399a0dbb571197db8d70b0e0f9ee99
Lisa <fhs45899@fh-salzburg.ac.at>
2024-04-12 11:28:26 +0200

    Add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,26 @@ module.exports = {
   ],
   theme: {
     colors: {
-      'blue': '#1d343f',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      'primary': '#5D15FF',
+      'white': '#FFFFFF',
+      'black': '#000000',
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      sans: 'Helvetica, sans-serif',
+    },
+    fontSize: {
+      'xs': '0.75rem',
+      'sm': '0.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      'h1': '8.125rem',
     },
     extend: {
       spacing: {
