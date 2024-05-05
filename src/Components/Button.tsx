5d54171fccee4be82c7ec804569b42e9f6b25105
Lisa <fhs45899@fh-salzburg.ac.at>
2024-05-05 14:29:25 +0200

    Adjust button component

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-type Props = {
-    path: string,
-    children?: React.ReactNode,
+type ButtonProps = {
+    onClick: () => void,
+    children: React.ReactNode,
 };
 
-export class Button extends React.Component<Props> {
-    render() {
-        return (
-            <Link to={this.props.path} className="flex justify-center items-center text-button min-w-40 p-1 border-solid border-2 rounded-full border-white">{this.props.children}</Link>
-        );
-    };
+export function Button({children, onClick}: ButtonProps) {
+    return (
+        <button
+            className="flex justify-center items-center text-button min-w-40 p-1 border-solid border-2 rounded-full border-white hover:bg-white hover:text-black"
+            onClick={onClick}>{children}</button>
+    );
 };
